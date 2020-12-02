1019589f459f6d81e05ba8532fbac6b0b47a6497
lijunpeng71 <lijunpeng71@163.com>
2020-12-02 15:40:33 +0800

    [dev] init

--- designpattern-java-study/src/main/java/com/designpattern/study/principle/demeter/improve/Demeter1.java
+++ designpattern-java-study/src/main/java/com/designpattern/study/principle/demeter/improve/Demeter1.java
@@ -50,7 +50,7 @@ class CollegeEmployee {
 class CollegeManager {
 	//返回学院的所有员工
 	public List<CollegeEmployee> getAllEmployee() {
-		List<CollegeEmployee> list = new ArrayList<CollegeEmployee>();
+		List<CollegeEmployee> list = new ArrayList<>();
 		for (int i = 0; i < 10; i++) { //这里我们增加了10个员工到 list
 			CollegeEmployee emp = new CollegeEmployee();
 			emp.setId("学院员工id= " + i);
@@ -77,7 +77,7 @@ class CollegeManager {
 class SchoolManager {
 	//返回学校总部的员工
 	public List<Employee> getAllEmployee() {
-		List<Employee> list = new ArrayList<Employee>();
+		List<Employee> list = new ArrayList<>();
 		
 		for (int i = 0; i < 5; i++) { //这里我们增加了5个员工到 list
 			Employee emp = new Employee();
